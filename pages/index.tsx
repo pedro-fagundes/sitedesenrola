66cb16b758a0999d86c0e4f644e96abb41540c50
Pedro Fagundes <pedro05fagundes@gmail.com>
2023-10-17 15:11:40 -0300

    v18

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,9 +35,9 @@ function Home() {
   }, [scrolling]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-4 xl:py-16 px-4 xl:px-20 bg-gray-900 text-white">
+    <div className="flex flex-col items-center justify-center min-h-screen py-4 xl:py-8 px-4 xl:px-20 bg-gray-900 text-white">
       <div>
-        <h1 className="text-4xl xl:text-7xl font-bold my-2.5 text-center px-4 xl:px-20 bg-indigo-800 rounded p-2 xl:p-4 mx-auto shadow-2xl">
+        <h1 className="text-4xl xl:text-7xl font-bold my-2.5 text-center px-4 xl:px-20 bg-indigo-800 rounded p-2 xl:p-4 mx-auto shadow-2xl mb-8">
           Experiência UX/UI
         </h1>
       </div>
@@ -72,42 +72,45 @@ function Home() {
         </Carousel>
       </div>
 
-      <h2 className="mt-10 text-4xl xl:text-6xl font-bold text-center px-3 xl:px-6 bg-indigo-800 rounded p-3 xl:p-6 mx-auto shadow-lg">
-        Foco na Experiência e Interface do Usuário
-      </h2>
+      <div className="">
+        <h2 className="mt-10 text-4xl xl:text-8xl font-bold text-center px-3 xl:px-6 py-3 xl:py-6 bg-indigo-800 rounded mx-auto shadow-2xl">
+          Foco na Experiência e Interface do Usuário
+        </h2>
 
-      <p className="mt-6 text-lg xl:text-2xl text-center px-3 xl:px-6 bg-gray-800 rounded p-3 xl:p-6 mx-auto shadow-lg max-w-2xl">
-        Nossa abordagem de design centrada no usuário garante uma navegação engajadora e imersiva, além de uma interface intuitiva, agradável e totalmente voltada às necessidades dos usuários de dispositivos móveis.
-      </p>
+        <p className="mt-6 text-lg xl:text-3xl text-center px-3 xl:px-6 py-3 xl:py-6 bg-gray-800 rounded mx-auto shadow-lg">
+          Nossa abordagem de design centrada no usuário garante uma navegação engajadora e imersiva, além de uma interface intuitiva, agradável e totalmente voltada às necessidades dos usuários de dispositivos móveis.
+        </p>
 
-      <p className="mt-6 text-lg xl:text-2xl text-center px-3 xl:px-6 bg-gray-800 rounded p-3 xl:p-6 mx-auto shadow-lg max-w-2xl">
-        A Desenrola IFSC não é apenas mais um aplicativo. É uma experiência envolvente, útil, inovadora e feita para simplificar e organizar a vida acadêmica dos estudantes. Informações essenciais da rotina acadêmica estão agora em um único lugar – tornando a experiência acadêmica mais leve, inclusiva e eficiente.
-      </p>
+        <p className="mt-6 text-lg xl:text-3xl text-center px-3 xl:px-6 py-3 xl:py-6 bg-gray-800 rounded mx-auto shadow-lg">
+          A Desenrola IFSC não é apenas mais um aplicativo. É uma experiência envolvente, útil, inovadora e feita para simplificar e organizar a vida acadêmica dos estudantes. Informações essenciais da rotina acadêmica estão agora em um único lugar – tornando a experiência acadêmica mais leve, inclusiva e eficiente.
+        </p>
 
-      <div className="mt-10 mb-5 w-full xl:max-w-xl px-8 bg-gray-800 p-6 rounded-lg shadow-lg">
-        <h3 className="text-2xl mb-3">Pré-Salve o App</h3>
-        <p className="mb-3">Garanta o acesso antecipado ao nosso app! Clique no botão abaixo para registrar o seu interesse.</p>
-        <a
-          href="https://forms.gle/ZxRnB9WiTAWvr9638"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-white bg-red-600 hover:bg-red-700 px-5 py-3 rounded-lg shadow-lg text-lg xl:text-2xl font-bold transition-colors duration-200 block w-full text-center"
-        >
-          Pre-Save
-        </a>
-      </div>
 
-      <div className="mb-10 w-full xl:max-w-xl px-8 bg-gray-800 p-6 rounded-lg shadow-lg">
-        <h3 className="text-2xl mb-3">Envie sua Sugestão</h3>
-        <p className="mb-3">Sua opinião é muito importante para nós. Clique no botão abaixo para enviar suas sugestões para a nossa equipe.</p>
-        <a
-          href="https://forms.gle/ZTAc3Kh8wFeWjjH66"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-white bg-red-600 hover:bg-red-700 px-5 py-3 rounded-lg shadow-lg text-lg xl:text-2xl font-bold transition-colors duration-200 block w-full text-center"
-        >
-          Sugestão
-        </a>
+        <div className="mt-10 xl:mt-20 mb-5 xl:mb-10 w-full xl:w-auto bg-gray-800 p-6 xl:p-12 rounded-lg shadow-lg">
+          <h3 className="text-2xl xl:text-4xl mb-3 xl:mb-6">Pré-Salve o App</h3>
+          <p className="mb-3 xl:mb-6 text-lg xl:text-2xl">Garanta o acesso antecipado ao nosso app! Clique no botão abaixo para registrar o seu interesse.</p>
+          <a
+            href="https://forms.gle/ZxRnB9WiTAWvr9638"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white bg-red-600 hover:bg-red-700 px-5 xl:px-10 py-3 xl:py-6 rounded-lg shadow-lg text-lg xl:text-2xl font-bold transition-colors duration-200 block w-full text-center"
+          >
+            Pre-Save
+          </a>
+        </div>
+
+        <div className="mb-10 w-full xl:w-auto bg-gray-800 p-6 xl:p-12 rounded-lg shadow-lg">
+          <h3 className="text-2xl xl:text-4xl mb-3 xl:mb-6">Envie sua Sugestão</h3>
+          <p className="mb-3 xl:mb-6 text-lg xl:text-2xl">Sua opinião é muito importante para nós. Clique no botão abaixo para enviar suas sugestões para a nossa equipe.</p>
+          <a
+            href="https://forms.gle/ZTAc3Kh8wFeWjjH66"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white bg-red-600 hover:bg-red-700 px-5 xl:px-10 py-3 xl:py-6 rounded-lg shadow-lg text-lg xl:text-2xl font-bold transition-colors duration-200 block w-full text-center"
+          >
+            Sugestão
+          </a>
+        </div>
       </div>
     </div>
   );
